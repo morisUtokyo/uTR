331a3e0e557e13e5cef9e489322b42efbbeded1c
Shinichi Morishita <moris@edu.k.u-tokyo.ac.jp>
2022-08-29 16:20:59 +0900

    Test the accuracy of uTR and RepeatMasker
    This pipeline tests the accuracy of uTR and RepeatMasker using benchmark data randomly generated from mosaic tandem repeat seed patterns. C programs in subdirectories must be compiled in advance. The pipeline generates benchmark data, applies uTR to the benchmark, checks the accuracy of uTR, applies RepeatMasker to the data, and tests the accuracy of RepeatMasker.

--- test_public/test.sh
+++ test_public/test.sh
@@ -3,48 +3,96 @@
 # executable modules
 uTR=../uTR
 gen_units=gendata/gen
-acc=testdata/acc
+check_uTR=check_uTR/acc
 
 num_TRs=1000
 error_ratio=0
 
 list=("AC_AG" "ACC_GTT" "CCTGGT_CTTTT" "CCTGGT_CTTTT_CTTGT" "AAG_AGG" "AAAG_AGG" "AAG_AG" "AAAG_AG" "AAAG_AG_AAAG" "AAAG_AG_AGGG_AG_AAAG" "AAAAGAAAGAGAGGG_AGGGG" "AGGGG_AAAAGAAAGAGAGGG_AGGGG")
 
+wallclock_uTR="time_uTR.txt"
+wallclock_RM="time_RM.txt"
+acc_table_uTR="accuracy_uTR.txt"
+acc_table_RepeatMasker="accuracy_RepeatMasker.txt"
+rm $wallclock $wallclock_RM $acc_table_uTR $acc_table_RepeatMasker
+
+mkdir tmp
+
 for units_name in ${list[@]}
 do
     units=${units_name//\_/ }
     echo $units
-
-    res="${units_name//[^_]}"
-    numUnits=$(( ${#res}+1 ))
-
-    TR_file=$units_name".fasta"
-    TR_decomp_file=$units_name"_decomp.fasta"
-    #echo $TR_file
-    
-    stat=$units_name"_unit_stat.txt"
-    wallclock=$units_name"_time.txt"
-    acc_table=$units_name"_accuracy.txt"
-    rm $stat
-    rm $wallclock
-    rm $acc_table
-    echo "Units       =" $units > $acc_table
-    echo "Num. TRs    = " $num_TRs   >> $acc_table
-    echo "Error ratio = " $error_ratio   >> $acc_table
+    echo $units >> $wallclock_uTR
+    echo $units >> $wallclock_RM
+    echo $units >> $acc_table_uTR
+    echo $units >> $acc_table_RepeatMasker
 
     min_a=(2)
-    max_a=(3 5 10 20 50 100 200 500)
+    max_a=(20 50 100 200 500)
+    #max_a=(3 5 10 20 50 100 200 500)
 
     for min_unit_occ in ${min_a[@]}
     do
         for max_unit_occ in ${max_a[@]}
         do
-            $gen_units -k $min_unit_occ -l $max_unit_occ -n $num_TRs -e $error_ratio -m $numUnits $units > $TR_file
+            run_name=$units_name"_"$min_unit_occ"_"$max_unit_occ
+            echo $run_name
+            param=$min_unit_occ" "$max_unit_occ"\t"
             
-            $uTR -f $TR_file -std -o $TR_decomp_file 1>> $stat 2>> $wallclock
+            # Generate data of tandem repeats for the given pattern
+            TR_file=$run_name".fasta"
+            res="${units_name//[^_]}"
+            numUnits=$(( ${#res}+1 ))
+            #echo $gen_units $min_unit_occ $max_unit_occ $num_TRs $error_ratio $numUnits $units $TR_file
+            $gen_units -k $min_unit_occ -l $max_unit_occ -n $num_TRs -e $error_ratio -m $numUnits $units > $TR_file
+            # Run uTR
+            TR_uTR_decomp_file=$run_name"_decomp_uTR.fasta"
+            uTR_stat=$run_name"_unit_stat_uTR.txt"
+            
+            echo -n -e $param >> $wallclock_uTR
+            $uTR -f $TR_file -std -o $TR_uTR_decomp_file 1>> $uTR_stat 2>> $wallclock_uTR
+            rm $uTR_stat
+            # Compute the accuracy of uTR
+            echo -n -e $param >> $acc_table_uTR #"Min Max num unit occ"
+            $check_uTR $TR_file $TR_uTR_decomp_file >> $acc_table_uTR
+            
+            # Run RepeatMasker
+            parse_RM=$run_name".fasta.result.txt"
+            match_RM=$run_name".fasta.match.txt"
+            
+            # Apply RepeatMasker to the string set
+            SECONDS=0
+            repeatmasker -e hmmer -noint -pa 4 -div 0 -xsmall $TR_file &> progress_report.txt
+            run_time=$SECONDS
+            echo -n -e $param >> $wallclock_RM
+            echo $run_time >> $wallclock_RM
+            
+            #repeatmasker -e hmmer -noint -pa 4 -div 20 -xsmall $TR_file &> progress_report.txt
+            # -e(ngine) [crossmatch|wublast|abblast|ncbi|rmblast|hmmer]
+            # -noint Only masks low complex/simple repeats (no interspersed repeats)
+            # -pa 4 The number of sequence batch jobs [50kb minimum] to run in parallel.
+            # -div [number] Masks only those repeats < x percent diverged from consensus seq
+            # -xsmall Returns repetitive regions in lowercase (rest capitals) rather than masked
+            parse_RepeatMasker/parseRM -i $TR_file.out -o $parse_RM
+            # Parse a repeatmasker .out file and output the result
+            # Remove temporary files
+            rm $TR_file.cat
+            rm $TR_file.tbl
+            rm $TR_file.out
+            rm $TR_file.masked
+            rm progress_report.txt
 
-            echo -n $min_unit_occ $max_unit_occ " " >> $acc_table #"Min Max num unit occ"
-            $acc $TR_file $TR_decomp_file >> $acc_table
+            # Parse the file to determine how RepeatMasker could identify mosaic tandem repeats
+            check_RepeatMasker/checkRM -i $parse_RM -p > $match_RM
+            
+            # Print out the statistics
+            echo -n -e $param >> $acc_table_RepeatMasker
+            tail -n1 $match_RM >> $acc_table_RepeatMasker
+            
+            mv $TR_file  tmp
+            mv $TR_uTR_decomp_file tmp
+            mv $parse_RM tmp
+            mv $match_RM tmp
         done
     done
 done

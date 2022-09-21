d4428205a1f5baf9421ba8e59339fd9be96af9b6
Shinichi Morishita <moris@edu.k.u-tokyo.ac.jp>
2022-09-21 18:51:20 +0900

    Add files via upload

---
+++ test_public/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd check_uTR; make clean; make;
+cd ../gendata; make clean; make;
+cd ../parse_RepeatMasker; make clean; make;
+cd ../check_RepeatMasker; make clean; make;
+cd ..
+
+
+exit 0
